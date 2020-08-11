626b4476591cc67f2b697f5195177b6544c6597f
RobPerez155 <robert.perez.psirho@gmail.com>
2020-08-11 09:20:16 -0400

    Commit Prior to Configuring Codeship

--- app/javascript/react/components/App.js
+++ app/javascript/react/components/App.js
@@ -1,7 +1,21 @@
 import React from 'react'
 
-export const App = (props) => {
-  return (<h1>Wine Mate United States</h1>)
+import { Route, Switch, BrowserRouter } from "react-router-dom"
+
+import RegionsIndexPage from "./RegionsIndexPage"
+
+const App = (props) => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={RegionsIndexPage} />
+      </Switch>
+    </BrowserRouter>
+  )
 }
+
+// export const App = (props) => {
+//   return (<h1>Wine Mate United States</h1>)
+// }
 
 export default App
